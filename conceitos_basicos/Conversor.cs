5496600f0ae50e7556021c26792a2ecf9fd02ee4
2022101384 <2022101384@LAB2-007>
2023-08-22 20:08:30 -0300

    Adicionar arquivos de projeto.

---
+++ conceitos_basicos/Conversor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conceitos_basicos
+{
+    public class Conversor
+    {
+        public static double MetrosMilimetros(double metros)
+        {
+            return metros * 1000;
+        }
+
+    }
+}
---
+++ conceitos_basicos/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using conceitos_basicos;
+
+//int num1 = 20;
+//int num2 = 11, result;
+//somador soma;
+//soma = new somador();
+
+//result = soma.Soma(num1, num2);
+
+//Console.WriteLine($"A soma dos numeros {num1} + {num2} é {result}");
+
+
+
+
+//Conversor conversor = new Conversor();
+//double metros = 10;
+//onsole.WriteLine("A medida {0}  metros coresponde a {1} milimetros", metros, Conversor.MetrosMilimetros(10));
+
